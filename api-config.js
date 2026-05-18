b4a077a9836cdb38b3b622332ba9a79343b51675
Gabriel <gabriel085srx@gmail.com>
2026-05-17 22:10:12 -0300

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,7 @@
+const API = {
+    SERVER: 'http://primegold.site',
+    USER: 'vodsapi-pro',
+    PASS: '401963597'
+};
+
+export default API;
---
+++ vods.js
@@ -0,0 +1,25 @@
+import API from './api-config.js';
+
+export default async function handler(req, res) {
+    const { action, category_id, vod_id } = req.query;
+    const BASE = `${API.SERVER}/player_api.php?username=${API.USER}&password=${API.PASS}`;
+
+    try {
+        let url;
+        if (action === 'categories') {
+            url = `${BASE}&action=get_vod_categories`;
+        } else if (action === 'streams') {
+            url = `${BASE}&action=get_vod_streams&category_id=${category_id}`;
+        } else if (action === 'info') {
+            url = `${BASE}&action=get_vod_info&vod_id=${vod_id}`;
+        }
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        res.setHeader('Cache-Control', 'public, max-age=3600');
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar dados' });
+    }
+}
